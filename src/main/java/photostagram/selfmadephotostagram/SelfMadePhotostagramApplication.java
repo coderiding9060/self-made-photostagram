f077750e1b36f3787fa0fa14529726e63fe41840
HOME <coderiding9060@gmail.com>
2021-10-03 21:22:03 +0900

    Initial commit

---
+++ src/main/java/photostagram/selfmadephotostagram/SelfMadePhotostagramApplication.java
@@ -0,0 +1,13 @@
+package photostagram.selfmadephotostagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfMadePhotostagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfMadePhotostagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/photostagram/selfmadephotostagram/SelfMadePhotostagramApplicationTests.java
@@ -0,0 +1,13 @@
+package photostagram.selfmadephotostagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfMadePhotostagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
