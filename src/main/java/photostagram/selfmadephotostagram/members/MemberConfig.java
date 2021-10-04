9ca83acb22fb8a118962c19cdd02411287fd7c8e
HOME <coderiding9060@gmail.com>
2021-10-05 03:36:51 +0900

    <feat>(<MemberConfig.java>): <회원 관련 Bean 관리 객체 추가>

--- src/main/java/photostagram/selfmadephotostagram/members/MemberConfig.java
+++ src/main/java/photostagram/selfmadephotostagram/members/MemberConfig.java
@@ -1,12 +1,15 @@
 package photostagram.selfmadephotostagram.members;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import photostagram.selfmadephotostagram.members.repository.JdbcMemberRepository;
 import photostagram.selfmadephotostagram.members.repository.MemberRepository;
 import photostagram.selfmadephotostagram.members.service.MemberService;
 import photostagram.selfmadephotostagram.members.service.MemberServiceImpl;
+import photostagram.selfmadephotostagram.members.util.SessionManageUtil;
 
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 @Configuration
@@ -14,8 +17,12 @@ public class MemberConfig {
 
     private DataSource dataSource;
 
-    public MemberConfig(DataSource dataSource) {
+    private HttpSession httpSession;
+
+    @Autowired
+    public MemberConfig(DataSource dataSource, HttpSession httpSession) {
         this.dataSource = dataSource;
+        this.httpSession = httpSession;
     }
 
     @Bean
@@ -26,5 +33,10 @@ public class MemberConfig {
     @Bean
     MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    SessionManageUtil sessionManageUtil(){
+        return new SessionManageUtil(httpSession);
     }
 }
