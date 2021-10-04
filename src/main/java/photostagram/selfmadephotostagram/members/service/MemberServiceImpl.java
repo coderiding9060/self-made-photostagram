7abc745b06780f216ac5e278649066ba32de8f93
HOME <coderiding9060@gmail.com>
2021-10-04 15:02:31 +0900

    <chore>(<MemberServiceImpl.java>): <@Transaction 어노테이션 추가>

--- src/main/java/photostagram/selfmadephotostagram/members/service/MemberServiceImpl.java
+++ src/main/java/photostagram/selfmadephotostagram/members/service/MemberServiceImpl.java
@@ -1,12 +1,14 @@
 package photostagram.selfmadephotostagram.members.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import photostagram.selfmadephotostagram.members.domain.Member;
 import photostagram.selfmadephotostagram.members.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberServiceImpl implements MemberService{
 
     private MemberRepository memberRepository;
